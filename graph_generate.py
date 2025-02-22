424586c2162ddf70a9a45be507a9172106ff0f1b
Lorenzo Lopez <geltbm@gmail.com>
2025-02-21 23:26:41 -0500

    feat: add relative lovasz theta calculator

--- graph_generate.py
+++ graph_generate.py
@@ -1,52 +1,54 @@
 # Parts of this file were copy-pasted from graph_generate.py by Colton Griffin and Aneesh Vinod Khilnani (https://github.com/chemfinal-dot/2021-Sinclair-Project)
 
 import numpy as np
-import random
 from typing import *
 
-def edges(graph: Dict[int, List[int]]):
-    """
-    Return the edge list of a graph and the edge list of the complementary graph
+class Graph:
+    def __init__(self, adj_list: Dict[int, List[int]]):
+        self._adj_list = adj_list
+        self._n = len(adj_list)
 
-    Input: Adjacency list
-    Note that if [i,j] is an edge, then [j,i] is also considered an edge.
-    """
-    edge = []
-    edge_complement = []
-    for node in graph:
-        for neighbour in graph[node]:
-            edge.append([node, neighbour])
-    for i in range(len(graph)):
-        for j in range(len(graph)):
-            if [i,j] not in edge and i != j:
-                edge_complement.append([i,j])
-    return edge, edge_complement
+    @classmethod
+    def from_edge_list(cls, n, *edge_list: List[Tuple[int, int]]):
+        adj_list = { i: [] for i in range(0, n) }
+        for (i, j) in edge_list:
+            adj_list[i].append(j)
+            adj_list[j].append(i)
+        return cls(adj_list)
 
-def complete_edges(graph: Dict[int, List[int]]):
-    """
-    Converts unidirectional edges of a graph to bidirectional
+    def n(self) -> int:
+        return self._n
 
-    Input: Adjacency list
-    """
+    def edges(self) -> Tuple[List[Tuple[int, int]], List[Tuple[int, int]]]:
+        """
+        Return the edge list of a graph and the edge list of the complementary graph
 
-    for x in range(len(graph)):
-        for i in range(len(graph)):
-            if i in graph[x] and x not in graph[i]:
-                graph[i].append(x)
-    return graph
+        Input: Adjacency list
+        Note that if [i,j] is an edge, then [j,i] is also considered an edge.
+        """
+        edge = []
+        edge_complement = []
+        for node in self._adj_list:
+            for neighbour in self._adj_list[node]:
+                edge.append([node, neighbour])
+        for i in range(self._n):
+            for j in range(self._n):
+                if [i,j] not in edge and i != j:
+                    edge_complement.append([i,j])
+        return edge, edge_complement
 
-def cycle(n: int):
+def cycle(n: int) -> Graph:
     """
     Creates a cycle graph of n nodes
     """
 
-    graph = dict(zip([i for i in range(n)], [[] for i in range(n)]))
+    adj_list = dict(zip([i for i in range(n)], [[] for i in range(n)]))
     for x in range(n):
-        graph[x].append((x+1) % n)
-        graph[x].append((x-1) % n)
-    return graph
+        adj_list[x].append((x+1) % n)
+        adj_list[x].append((x-1) % n)
+    return Graph(adj_list)
 
-def line(n: int):
+def line(n: int) -> Graph:
     """
     Creates a line graph of n nodes
     """
@@ -56,23 +58,27 @@ def line(n: int):
     elif n == 1:
         return {0: []}
 
-    graph = {0: [1], n - 1: {n - 2}}
+    adj_list = { 0: [1], n - 1: {n - 2} }
     for i in range(1, n - 1):
-        graph[i] = [i - 1, i + 1]
+        adj_list[i] = [i - 1, i + 1]
 
-    return graph
+    return Graph(adj_list)
 
-def dense_graph(n):
+def complete(n: int) -> Graph:
     """
-    Creates a graph where each possible edge is given a 50% chance of being assigned.
+    Creates a complete graph of n nodes
     """
 
-    graph = dict(zip([i for i in range(n)], [[] for i in range(n)]))
-    for x in range(n):
-        for i in range(x+1,n):
-            if random.random() < 0.8:
-                graph[x].append(i)
-    return complete_edges(graph)
+    adj_list = { i: [j for j in range(0, n) if j != i] for i in range(0, n) }
+    return Graph(adj_list)
+
+def independent(n: int) -> Graph:
+    """
+    Creates a graph of n nodes with no edges
+    """
+
+    adj_list = { i: [] for i in range(0, n)}
+    return Graph(adj_list)
 
 def e_matrix(n: int, i: int, j: int):
     """
@@ -95,19 +101,19 @@ def delta_matrix(n: int):
             Delta = np.add(Delta, np.kron(E,E))
     return Delta
 
-def adjacency_matrix(graph: Dict[int, List[int]]):
+def adjacency_matrix(graph: Graph):
     """
     Creates the adjacency matrix of a graph
 
     Input: Adjacency list
     """
 
-    n = len(graph)
-    edge_set, edge_complement = edges(graph)
+    n = graph._n
+    edges, nonedges = graph.edges
     E = np.zeros((n, n))
     E_complement = np.zeros((n, n))
-    for x in edge_set:
+    for x in edges:
         E[x[0], x[1]] = 1
-    for x in edge_complement:
+    for x in nonedges:
         E_complement[x[0], x[1]] = 1
     return E, E_complement
--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import numpy as np
 import picos
 from typing import *
 import graph_generate as gg
+from graph_generate import Graph
 
-def lovasz_theta(graph: Dict[int, List[int]]):
+def lovasz_theta(graph: Graph):
     """
     Calculate the Lovasz Theta number of a graph using SDP. Also returns the witness
 
@@ -11,13 +12,13 @@ def lovasz_theta(graph: Dict[int, List[int]]):
     Minimize t such that
     1. y_ij = -1        if not (i ~ j)
     2. y_ii = t - 1     for i from 1 to n
-    3. Y is PSD
+    3. Y is a PSD n by n matrix
 
     SDP taken from Theorem 3.6.1 in "Approximation Algorithms and Semidefinite Programming" by G\"artner and Matousek
     """
 
-    n = len(graph)
-    _, non_edges = gg.edges(graph)
+    n = graph.n()
+    _, non_edges = graph.edges()
 
     Y = picos.SymmetricVariable("Y", (n, n))
     t = picos.RealVariable("t")
@@ -33,7 +34,7 @@ def lovasz_theta(graph: Dict[int, List[int]]):
     prob.solve(solver="cvxopt")
     return t.value, Y.value
 
-def ind_mn_to_sg(graph: Dict[int, List[int]]):
+def ind_mn_to_sg(graph: Graph):
     """
     Computes Ind_CP(M_n : S_G), where S_G is the graph system corresponding to the input graph, which equals the Lovasz Theta of the graph
 
@@ -47,8 +48,8 @@ def ind_mn_to_sg(graph: Dict[int, List[int]]):
     Prop 4.4 proved that this quantity (Ind_CP(M_n : S_G)) equals the Lovasz theta of the graph
     """
 
-    n = len(graph)
-    _, non_edges = gg.edges(graph)
+    n = graph.n()
+    _, non_edges = graph.edges()
 
     X = picos.SymmetricVariable("X", (n*n, n*n))    # This should be a hermitian variable but cvxopt is crashing when you change it
     lam = picos.RealVariable("lambda")
@@ -57,7 +58,7 @@ def ind_mn_to_sg(graph: Dict[int, List[int]]):
     prob.objective = "max", lam
 
     # Constraint 1
-    prob.add_constraint(X.partial_trace(subsystems=-1, dimensions=n) == (1 - lam) * np.identity(n))
+    prob.add_constraint(X.partial_trace(subsystems=0, dimensions=n) == (1 - lam) * np.identity(n))
 
     # Constraint 2
     for i in range(0, n):
@@ -72,16 +73,87 @@ def ind_mn_to_sg(graph: Dict[int, List[int]]):
     prob.add_constraint(X >> 0)
 
     prob.solve(solver="cvxopt")
-    return 1/lam.value
+    return 1 / lam.value
 
+def ind_sg_to_sl(graph_g: Graph, graph_l: Graph) -> float:
+    """
+    Computes Ind_CP(S_G : S_L), where L is a subgraph of G and S_G and S_L are the graph systems corresponding to G and L, respectively
+
+    If graph_l has fewer vertices than graph_g, then it is "padded" with extra vertexes
+
+    This is the "relative Lovasz Theta" defined by Arazia, Griffin, and Sinclair.
+
+    SDP:
+    Maximize lam such that
+    1. (tr (x) id)(X) = (1 - lam)(I_n)
+    2. X + lam * delta_matrix_n \in (S_G (x) S_L) + (S_G^perp (x) M_n)
+    3. X is a PSD n^2 by n^2 matrix
+
+    SDP modified from the one given in prop 4.1.
+    """
+
+    n = graph_g.n()
+
+    _, non_edges_g = graph_g.edges()
+    _, non_edges_l = graph_l.edges()
+
+    X = picos.SymmetricVariable("X", (n*n, n*n))    # This should be a hermitian variable but cvxopt is crashing when you change it
+    lam = picos.RealVariable("lambda")
+
+    prob = picos.Problem()
+    prob.objective = "max", lam
+
+    # Constraint 1
+    prob.add_constraint(X.partial_trace(subsystems=0, dimensions=n) == (1 - lam) * np.identity(n))
+
+    # Constraint 2
+    for i in range(0, n):
+        for j in range(0, n):
+            if (i, j) in non_edges_g:
+                continue
+            for (edge_i, edge_j) in non_edges_l:
+                if (edge_i, edge_j) == (i, j):
+                    prob.add_constraint(X[i * n + edge_i, j * n + edge_j] + lam == 0)
+                else:
+                    prob.add_constraint(X[i * n + edge_i, j * n + edge_j] == 0)
+    
+    # Constraint 3 
+    prob.add_constraint(X >> 0)
+
+    prob.solve(solver="cvxopt")
+    return 1 / lam.value, X.value
+
+relative_lov_theta = ind_sg_to_sl
+
+def pretty_print(matrix, precision: int = 2):
+    width, height = matrix.size
+    for i in range(0, height):
+        for j in range(0, width):
+            val = 0 if (abs(matrix[i, j]) < 0.01) else matrix[i, j]
+            print(f"{val:.{precision}f}".rjust(6), end='')
+        print()
 
 def main():
-    print(f"Normal Lovasz Theta of L3: {lovasz_theta(gg.line(3))}")
+    # print(f"Normal Lovasz Theta of L3: {lovasz_theta(gg.line(3))}")
 
-    for i in range(2, 6):
-        print(f"IndCP Lovasz Theta of L{i}: {ind_mn_to_sg(gg.line(i))}")
+    # for i in range(2, 6):
+    #     print(f"IndCP Lovasz Theta of L{i}: {ind_mn_to_sg(gg.line(i))}")
         
-    print(f"IndCP Lovasz Theta of C5: {ind_mn_to_sg(gg.cycle(5))}")
+    # print(f"IndCP Lovasz Theta of C5: {ind_mn_to_sg(gg.cycle(5))}")
+
+    # print(f"Lovasz Theta of K5 : C5: {relative_lov_theta(gg.complete(5), gg.cycle(5))}")
+
+    for n in range(3, 6):
+        L_big = gg.line(n)
+        L2_sub_L_big = Graph.from_edge_list(n, (0, 1))
+        val, matrix = relative_lov_theta(L_big, L2_sub_L_big)
+        print(f"L2 : L{n} = {val}")
+        # pretty_print(matrix)
+    
+    g = Graph.from_edge_list(4, (0, 1), (1, 2), (1, 2), (2, 3))
+    L2_sub_g = Graph.from_edge_list(4, (0, 1))
+    L2_to_g, matrix = relative_lov_theta(g, L2_sub_g)
+    print(f"L2 : G = {L2_to_g}")
 
 if __name__ == "__main__":
     main()
